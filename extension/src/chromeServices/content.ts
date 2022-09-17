cd8d9bbf0fe1461a372c87ac5d24f7db3b2dbe13
Pileks <jure@amber-it.co>
2022-09-17 14:56:57 +0200

    extension fn call test using CustomEvent

--- extension/src/chromeServices/content.ts
+++ extension/src/chromeServices/content.ts
@@ -3,3 +3,10 @@ console.log("Test successful!");
 var s = document.createElement('script');
 s.src = chrome.runtime.getURL('static/js/inject.js');
 (document.head || document.documentElement).appendChild(s);
+
+document.addEventListener('korianderExt_test', function(e) {
+  // e.detail contains the transferred data (can be anything, ranging
+  // from JavaScript objects to strings).
+  // Do something, for example:
+  alert((e as CustomEvent<string>).detail);
+})
--- extension/src/chromeServices/inject.ts
+++ extension/src/chromeServices/inject.ts
@@ -1,5 +1,11 @@
 console.log("I INJECT!");
 
 (window as any).koriander = {
-  
+  test: (msg: string) => {
+    document.dispatchEvent(
+      new CustomEvent("korianderExt_test", {
+        detail: msg
+      })
+    );
+  },
 };
